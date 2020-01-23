062cff370b8751afeb5ad0b76849a48e53833704
Tatiana <awoltat@gmail.com>
2020-01-23 17:07:51 +0200

    add homework

--- HW_11/script.js
+++ HW_11/script.js
@@ -1,15 +1,16 @@
-
+//1
 async function promises() {
     let count = 2;
+
     function prom() {
-        return new Promise(function(resolve, reject) {
-            setTimeout(() => resolve( count = count * 2), 3000);
+        return new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(count = count * 2), 3000);
         });
     }
 
     function prom_second() {
-        return new Promise(function(resolve, reject) {
-            setTimeout(() => resolve( count = count + 1), 2000);
+        return new Promise(function (resolve, reject) {
+            setTimeout(() => resolve(count = count + 1), 2000);
         });
     }
 
@@ -19,5 +20,25 @@ async function promises() {
 
     // prom().then(value => prom_second().then(value1 => console.log(value1)));  второй вариант
 }
+
 promises();
+
+//2
+async function foo() {
+    let result = [];
+    const getBooks = async (n = 1) => {
+        let books = await fetch(`https://www.googleapis.com/books/v1/volumes?q=quilting=${n}`);
+        books = await books.json();
+        return books.items
+    };
+
+    for (let i = 1; i <= 5; i++) {
+        await getBooks(i).then(value => result = result.concat(value));
+    }
+    console.log(result);
+}
+
+foo();
+
+
 
